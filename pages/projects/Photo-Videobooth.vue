8969bba6e5ffeaa98b493a147faf95b4397c2850
Firman Adiyuda <firmanadiyuda@gmail.com>
2022-06-03 22:21:44 +0700

    fix bug tampilan

--- pages/projects/Photo-Videobooth.vue
+++ pages/projects/Photo-Videobooth.vue
@@ -3,9 +3,9 @@
         <Navbar />
         <div class="hero min-h-screen bg-primary">
             <div class="hero-content text-center z-40">
-                <div class="max-w-md">
+                <div class="max-w-lg">
                     <p class="text-xl tracking-[0.3em]"><b>OUR</b> PROJECTS</p>
-                    <h1 class="pb-8 pt-2 text-5xl font-bold">PHOTO/VIDEOBOOTH</h1>
+                    <h1 class="pb-8 pt-2 text-5xl font-bold">PHOTO / VIDEOBOOTH</h1>
                     <button class="btn btn-outline btn-disabled btn-accent">Coming Soon!</button>
                 </div>
             </div>

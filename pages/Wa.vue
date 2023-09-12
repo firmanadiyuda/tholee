db78e11c57309af3cf98ee346bb169c1a3bf5d44
firmanadiyuda <firmanadiyuda@gmail.com>
2023-09-12 18:28:49 +0700

    add redirect to wa page

---
+++ pages/Wa.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+useHead({
+    title: 'WhatsApp',
+})
+
+onMounted(() => {
+    window.location.href = "https://wa.me/6289525009655";
+});
+
+</script>
